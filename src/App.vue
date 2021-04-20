ec5b5617e58d2fde5c2cfd4bba3763a05702c5af
Valentin Tashkov <valentin.tashkov@gmail.com>
2021-04-20 17:09:14 +0300

    wip: global scss imprts

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script lang="ts">
-import { provide } from "vue";
-import { useStore } from "vuex";
-import { useRoute, useRouter } from "vue-router";
+import { provide } from "vue"
+import { useStore } from "vuex"
+import { useRoute, useRouter } from "vue-router"
 
 export default {
   setup() {
@@ -15,9 +15,13 @@ export default {
       $store: useStore(),
       $router: useRouter(),
       $route: useRoute(),
-    };
-    provide("context", context);
-    return {};
+    }
+    provide("context", context)
+    return {}
   },
-};
+}
 </script>
+
+<style lang="ts">
+@import "./assets/scss/_index.scss";
+</style>
